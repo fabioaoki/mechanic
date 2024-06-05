c24d855727b4d846a016da6918457e66264e5643
Fabio Aoki <fabio.aoki.93@gmail.com>
2024-06-05 12:16:22 -0300

    fix: add mais cores.

--- mechanic/src/main/java/br/com/mechanic/mechanic/service/ColorUtil.java
+++ mechanic/src/main/java/br/com/mechanic/mechanic/service/ColorUtil.java
@@ -14,6 +14,19 @@ public class ColorUtil {
         colorMap.put("amarelo", "#FFFF00");
         colorMap.put("preto", "#000000");
         colorMap.put("branco", "#FFFFFF");
+        colorMap.put("prata", "#C0C0C0");
+        colorMap.put("cinza", "#808080");
+        colorMap.put("marrom", "#A52A2A");
+        colorMap.put("bege", "#F5F5DC");
+        colorMap.put("roxo", "#800080");
+        colorMap.put("rosa", "#FFC0CB");
+        colorMap.put("laranja", "#FFA500");
+        colorMap.put("dourado", "#FFD700");
+        colorMap.put("bordô", "#800000");
+        colorMap.put("azul marinho", "#000080");
+        colorMap.put("verde oliva", "#808000");
+        colorMap.put("vinho", "#722F37");
+        colorMap.put("champanhe", "#F7E7CE");
     }
 
     public static boolean isValidColor(String color) {
