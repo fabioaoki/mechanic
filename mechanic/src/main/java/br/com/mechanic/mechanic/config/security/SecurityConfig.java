dfae232bd77ec27df19edddb3e07659dc3a78cdb
Fabio Aoki <fabio.aoki.93@gmail.com>
2024-11-05 17:58:41 -0300

    fix: Criando possibilidade de testes sem token.

--- mechanic/src/main/java/br/com/mechanic/mechanic/config/security/SecurityConfig.java
+++ mechanic/src/main/java/br/com/mechanic/mechanic/config/security/SecurityConfig.java
@@ -28,14 +28,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable())  // Desabilita CSRF para ser stateless
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))  // Sessão stateless
                 .addFilterBefore(new AuthenticationToTokenFilter(tokenServiceBo, repository), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests(auth -> auth
-                        .requestMatchers(String.valueOf(PathRequest.toStaticResources().atCommonLocations())).permitAll()  // Exemplo para permitir recursos estáticos
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/auth").permitAll()  // Permite o login sem autenticação
-                        .anyRequest().authenticated());
+                                .requestMatchers(String.valueOf(PathRequest.toStaticResources().atCommonLocations())).permitAll()  // Exemplo para permitir recursos estáticos
+                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()  // Permite acesso ao Swagger UI e API Docs
+                                .requestMatchers("/auth").permitAll()  // Permite o login sem autenticação
+
+                        // Descomente a linha abaixo para habilitar a autenticação para todos os outros endpoints
+                        // .anyRequest().authenticated()  // Todos as outras requisições precisam de autenticação
+                );
 
         return http.build();
     }
